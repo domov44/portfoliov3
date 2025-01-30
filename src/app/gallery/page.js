272e65a40771d9e86e1ea371c8034c2bca4cc3e0
Ronan Scotet <102806074+domov44@users.noreply.github.com>
2025-01-30 11:30:37 +0100

    fix(deploy): json

--- src/app/gallery/page.js
+++ src/app/gallery/page.js
@@ -28,7 +28,7 @@ export default async function Page() {
                 items.push({
                     ...originalGallery,
                     id: `${originalGallery.id}-duplicate-${items.length}`,
-                    place: `${originalGallery.place}`,
+                    place: `${originalGallery.place} (${Math.floor(items.length / galleriesElements.length) + 1})`,
                     date: originalGallery.date,
                     url: originalGallery.url,
                     alt: originalGallery.alt
