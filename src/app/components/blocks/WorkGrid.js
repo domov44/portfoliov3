5919e9b3f91ffc74fe0b31fd86015520e0628dc8
Ronan Scotet <102806074+domov44@users.noreply.github.com>
2025-01-31 20:20:36 +0100

    feat(home): work grid

--- src/app/components/blocks/WorkGrid.js
+++ src/app/components/blocks/WorkGrid.js
@@ -1,28 +1,28 @@
 "use client";
 import React from 'react';
-import Title from '../../ui/textual/Title';
-import Stack from '../../ui/wrapper/Stack';
-import Section from '../../ui/wrapper/Section';
-import Container from '../../ui/wrapper/Container';
-import Button from '../../ui/button/Button';
-import styles from './HomeGrid.module.css';
-import InvisibleLink from '../../ui/button/InvisibleLink';
+import Title from '../ui/textual/Title';
+import Stack from '../ui/wrapper/Stack';
+import Section from '../ui/wrapper/Section';
+import Container from '../ui/wrapper/Container';
+import Button from '../ui/button/Button';
+import styles from './WorkGrid.module.css';
+import InvisibleLink from '../ui/button/InvisibleLink';
 
-const HomeGrid = ({ videos }) => {
+const WorkGrid = ({ videos, heading, button }) => {
   return (
     <Section fullWidth>
       <Container direction="column" width="100%" maxwidth="1200px">
         <Stack width="100%" justify="center">
           <Title level={2} className="colored font8vw zIndex_-1 text_align_center w60vw ln0_8">
-            PROJECT SHOWCASE
+            {heading}
           </Title>
         </Stack>
         <div className={styles.projectGrid}>
           {videos.map((video, index) => (
-            <article key={index} className={styles.gridItem}>
+            <article key={video.id || video.slug} className={styles.gridItem}>
               <video
                 className={styles.video}
-                src={video.video}
+                src={video.video.length > 0 ? video.video[0].mediaItemUrl : ''}
                 autoPlay
                 loop
                 muted
@@ -33,11 +33,11 @@ const HomeGrid = ({ videos }) => {
           ))}
         </div>
         <Stack width="100%" justify="center" margin="-30vw 0 0 0">
-          <Button transition className="step-1" href="/work">all projects</Button>
+          <Button transition className="step-1" href={button.url}>{button.title}</Button>
         </Stack>
       </Container>
     </Section>
   );
 };
 
-export default HomeGrid;
+export default WorkGrid;
