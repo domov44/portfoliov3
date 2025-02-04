5d0e500983bf67c42241b797b9ed26bae6696de3
Ronan Scotet <102806074+domov44@users.noreply.github.com>
2025-02-04 22:07:09 +0100

    feat(responsive): home hero responsive

--- src/app/components/blocks/HomeHero.js
+++ src/app/components/blocks/HomeHero.js
@@ -189,17 +189,19 @@ const HomeHero = ({ images, centerText, rightUrl, heading, leftUrl }) => {
         <Hero ref={heroRef}>
             <ImagesList images={images} />
             <Stack direction={"column"} width={"100%"} height={"calc(70vh - 80px)"} justify={"space-between"}>
-                <Stack justify={"space-between"} width={"100%"} zIndex={2} animationType={"animateFadeIn"} opacity={"0"}>
-                    <Stack width={"33.3%"}>
-                        <TextLink href={leftUrl.url} target={leftUrl.url.startsWith('https://') ? '_blank' : undefined}>{leftUrl.title}</TextLink>
-                    </Stack>
-                    <Stack width={"33.3%"} justify={"center"}>
+                <div className={styles.home_hero_content}>
+                    <div className={styles.home_hero_title}>
                         <Title level={6} className="text_align_center default">{centerText}</Title>
-                    </Stack>
-                    <Stack width={"33.3%"} justify={"end"}>
-                        <TextLink href={rightUrl.url} target={rightUrl.url.startsWith('https://') ? '_blank' : undefined}>{rightUrl.title}</TextLink>
-                    </Stack>
-                </Stack>
+                    </div>
+                    <ul className={styles.home_hero_link}>
+                        <li className={styles.home_hero_link_item}>
+                            <TextLink href={leftUrl.url} target={leftUrl.url.startsWith('https://') ? '_blank' : undefined}>{leftUrl.title}</TextLink>
+                        </li>
+                        <li className={styles.home_hero_link_item}>
+                            <TextLink href={rightUrl.url} target={rightUrl.url.startsWith('https://') ? '_blank' : undefined}>{rightUrl.title}</TextLink>
+                        </li>
+                    </ul>
+                </div>
                 <Stack overflow={"hidden"} justify={"end"} width={"100%"}>
                     <Title ref={titleRef} level={1} data_cy="name-surname" className="colored hidden font14vw zIndex_-1">
                         {titleText.split('').map((letter, index) => (
